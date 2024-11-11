2ecac74f6a3e4e952ce5244e1319a8eaa1584d55
filippemonteiro <filippemonteiro@outook.com>
2024-11-11 14:31:36 -0300

    Upload do site designar

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+ * Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+ */
+
+document.querySelectorAll('.navbar-nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      const offset = 85; // Ajuste este valor conforme necessário
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = targetElement.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Adicione este trecho para lidar com o botão "Início" e a logo
+document.querySelectorAll('.navbar-nav a[href="#"], .navbar-brand').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      // Defina a altura da janela de visualização
+      const viewportHeight = window.innerHeight;
+      // Pega o elemento de cabeçalho (header)
+      const header = document.querySelector('#inicio'); // Certifique-se de que o ID do cabeçalho esteja correto
+      // Calcula a posição desejada do cabeçalho (centralizada)
+      const headerPosition = header.getBoundingClientRect().top + window.scrollY - (viewportHeight / 2 - header.offsetHeight / 2);
+
+      window.scrollTo({
+          top: headerPosition,
+          behavior: 'smooth'
+      });
+  });
+});
