2b09981acb98de9eeff3cc64919944a89d067679
filippemonteiro <filippemonteiro@outook.com>
2024-11-11 15:01:52 -0300

    Correção de erros

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
  */
 
-document.querySelectorAll('.navbar-nav a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetElement = document.querySelector(this.getAttribute('href'));
+document.querySelectorAll('.navbar-nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    // Verifica se o href é apenas "#"
+    if (href !== "#") {
+      const targetElement = document.querySelector(href);
       const offset = 85; // Ajuste este valor conforme necessário
       const bodyRect = document.body.getBoundingClientRect().top;
       const elementRect = targetElement.getBoundingClientRect().top;
@@ -15,26 +19,38 @@ document.querySelectorAll('.navbar-nav a[href^="#"]').forEach(anchor => {
       const offsetPosition = elementPosition - offset;
 
       window.scrollTo({
-          top: offsetPosition,
-          behavior: 'smooth'
+        top: offsetPosition,
+        behavior: "smooth",
       });
+    } else {
+      // Para href="#", rola até o topo da página
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   });
 });
 
 // Adicione este trecho para lidar com o botão "Início" e a logo
-document.querySelectorAll('.navbar-nav a[href="#"], .navbar-brand').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document
+  .querySelectorAll('.navbar-nav a[href="#"], .navbar-brand')
+  .forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
       e.preventDefault();
       // Defina a altura da janela de visualização
       const viewportHeight = window.innerHeight;
       // Pega o elemento de cabeçalho (header)
-      const header = document.querySelector('#inicio'); // Certifique-se de que o ID do cabeçalho esteja correto
+      const header = document.querySelector("#inicio"); // Certifique-se de que o ID do cabeçalho esteja correto
       // Calcula a posição desejada do cabeçalho (centralizada)
-      const headerPosition = header.getBoundingClientRect().top + window.scrollY - (viewportHeight / 2 - header.offsetHeight / 2);
+      const headerPosition =
+        header.getBoundingClientRect().top +
+        window.scrollY -
+        (viewportHeight / 2 - header.offsetHeight / 2);
 
       window.scrollTo({
-          top: headerPosition,
-          behavior: 'smooth'
+        top: headerPosition,
+        behavior: "smooth",
       });
+    });
   });
-});
